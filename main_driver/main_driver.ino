8f4a17b35dfcfe190d26b536b0c8c7f42704a7a4
RobKlock <rklock@oberlin.edu>
2023-02-20 13:28:39 -0600

    adjusting HTTP requests

--- main_driver/main_driver.ino
+++ main_driver/main_driver.ino
@@ -8,6 +8,7 @@
 #include <WiFiServer.h>
 #include <WiFiType.h>
 #include <WiFiUdp.h>
+#include <ArduinoJson.h>
 
 #include <HTTPClient.h>
 #include <SPI.h>
@@ -33,9 +34,11 @@ const char* ssid = SECRET_SSID; // Fill in real value in arduino_secrets.h
 const char* password = SECRET_PASSWORD;
 // Note: It takes a minute or two to connect. Personal phone hotspot works best
 const char* host = "maker.ifttt.com";
-const char* trigger = SECRET_TRIGGER;
-const char* apiKey = SECRET_APIKEY;
+char* trigger = SECRET_TRIGGER;
+char* apiKey = SECRET_APIKEY;
 
+WiFiClient client;
+void send_webhook();
 
 void setup() {
   // put your setup code here, to run once:
@@ -52,6 +55,29 @@ void setup() {
       Serial.println("Can't Connect");
     }
     Serial.println("Wifi Connected");
+    Serial.println("pushed"); // Serial monitor debugging
+
+  
+      HTTPClient http;
+
+      const int httpPort = 80;
+      if (!client.connect(host, httpPort)) {
+        return;
+      }
+
+      String url = SECRET_TRIGGER;
+      http.begin(client, url);
+  
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      
+      String httpRequestData = "";           
+      
+      int httpResponseCode = http.POST(httpRequestData);
+      http.end();
+      // Send Webook to IFTTT
+      Serial.println(httpResponseCode);
+      send_webhook();
+     
 }
 float getSonar() {
 	unsigned long pingTime;
@@ -75,26 +101,29 @@ void loop() {
   // if the object is closer than DISTANCE_THRESHOLD cm OR button is pressed, turn off the LED; otherwise, turn on the LED
   if( sonar_distance <= DISTANCE_THRESHOLD || button_state==LOW ) {
     digitalWrite(PIN_LED, LOW); // turn off the light
-    // Serial.println("pushed"); // Serial monitor debugging
-
-    WiFiClient client;
-      HTTPClient http;
-
-      const int httpPort = 80;
-      if (!client.connect(host, httpPort)) {
-        return;
-      }
-
-      // String url = host + trigger + apiKey;
-      // http.begin(client, url);
-      //code fails here
-      // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // String httpRequestData = "api_key=" + apiKey;           
-      // int httpResponseCode = http.POST(httpRequestData);
-      // http.end();
-
   }
   else 
     digitalWrite(PIN_LED, HIGH); // turn on the light
   }
 
+
+void send_webhook(){
+  Serial.println("Entered Send Webhook");
+  // construct the JSON payload
+  String jsonString = "";
+  jsonString += "{\"value1\":\"";
+  jsonString += "test";
+  jsonString += "\"}";
+  int jsonLength = jsonString.length();  
+  String lenString = String(jsonLength);
+  // connect to the Maker event server
+  client.connect("maker.ifttt.com", 80);
+  // construct the POST request
+  String url = SECRET_TRIGGER;
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "User-Agent: BuildFailureDetectorESP32\r\n" +
+               "Connection: close\r\n\r\n");
+  delay(500);
+  client.stop();
+}
