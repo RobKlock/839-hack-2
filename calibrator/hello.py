5357bca89c0e9e8c9ffc9468612ea971c61450e8
Gabriel Selzer <gjselzer@wisc.edu>
2023-03-19 12:27:26 -0500

    Add sensor data state to the website

--- calibrator/hello.py
+++ calibrator/hello.py
@@ -1,12 +1,21 @@
-from flask import Flask, request
+from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
-@app.route("/test", methods=["POST"])
-def test():
-    json = request.get_json()
-    sonar = json['sonar']
-    button = json['bed_sensor']
-    return f"Sonar: {sonar}, Button: {button}\n"
+# Shamelessly copied from https://github.com/pallets/flask/tree/2.0.3/examples/javascript
 
-if __name__ == '__main__':
-   app.run(debug = True)
+sonar_data = []
+sensor_data = []
+
+@app.route("/")
+def index():
+    return render_template(f"calibrator.html")
+
+
+@app.route("/calibrate", methods=["POST"])
+def calibrate():
+    sonar = request.form.get("sonar", 0, type=float)
+    bed_sensor = request.form.get("bed_sensor", False, type=bool)
+    sonar_data.append(sonar)
+    sensor_data.append(bed_sensor)
+    print({"sonar": sonar_data, "bed_sensor": sensor_data})
+    return jsonify(sonar_data=str(sonar_data), sensor_data=str(sensor_data))
