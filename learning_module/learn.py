660fb61077ccc0f8d8f7fe936309dacd5af37f9f
Samarth Mathur <samarthmathur93@gmail.com>
2023-03-19 15:40:13 -0500

    Added preprocessing and learning code

--- learning_module/learn.py
+++ learning_module/learn.py
@@ -22,7 +22,7 @@ def download_data(urlstring='http://3.138.135.239:3000',forced=False):
 def process_inputs(raw_data,features=['sonar','bed_sensor']):
     '''
     Function to process data in json dump
-    input: json file containing raw sensor data
+    input: list of dicts
     return:
         data: list of size (n,d) where n is the number of data points each of d dimensions
         labels: list of size n of 0-1 labels
@@ -33,23 +33,30 @@ def process_inputs(raw_data,features=['sonar','bed_sensor']):
     required_inputs=['labels']+features
     for line in raw_data:
         d=dict(line)
+        skip=False
         for feature in required_inputs:
             if feature not in d:
-                continue
+                skip=True
+                break
+        if not skip:
+            print("Adding",d.keys())
             clean_data.append(d)
 
     #data=[sonar, button]
     data=[]
     labels=[]
-    for data in clean_data:
-        labels.append(data['labels'])
-        datum=[]
-        for key in features
+    for x in clean_data:
+        N=min([len(x[feat]) for feat in required_inputs])
+        for i in range(N):
+            labels.append(x['labels'][i])
+            datum=[]
+            for feat in features:
+                datum.append(x[feat][i])
+            data.append(datum)
 
+    return(data,labels)
 
-    return(clean_data)
-
-class DecisionTree:
+class DecisionTreeModel:
     def __init__(self):
         self.clf = tree.DecisionTreeClassifier()
     def learn(self,data,labels):
@@ -59,7 +66,7 @@ class DecisionTree:
     def predict_probabilities(self,data):
         return self.lf.predict_proba(data)
 
-class LogisticRegression:
+class LogisticRegressionModel:
     def __init__(self):
         self.clf = LogisticRegression(random_state=0)
     def learn(self,data,labels):
@@ -73,9 +80,11 @@ def main():
     print("I do the learning!")
 
     raw_data=download_data()
-    data,labels=process_inputs(raw_data)
-
-
+    data,labels=process_inputs(raw_data[-8])
+    Tree=DecisionTreeModel()
+    Tree.learn(data,labels)
+    LR=LogisticRegressionModel()
+    LR.learn(data,labels)
     
 
 if __name__ == "__main__":
