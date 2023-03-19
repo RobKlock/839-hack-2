a308f652e687f48ab22f5250340120f7e87a62a7
Gabriel Selzer <gjselzer@wisc.edu>
2023-03-19 16:43:08 -0500

    Write data sampling code

--- calibrator/hello.py
+++ calibrator/hello.py
@@ -1,21 +1,81 @@
+from typing import List
 from flask import Flask, jsonify, render_template, request
+import requests
 app = Flask(__name__)
 
 # Shamelessly copied from https://github.com/pallets/flask/tree/2.0.3/examples/javascript
 
-sonar_data = []
-sensor_data = []
+# Store sensor data in arrays, for now.
+sonar_data: List[float] = []
+button_data: List[int] = []
+labels: List[int] = []
+
+# Calibration state
+learning_complete: bool = False
+samples_per_label: int = 10
+current_label: bool = False
+samples_remaining: int = samples_per_label
 
 @app.route("/")
 def index():
     return render_template(f"calibrator.html")
 
 
+def learn():
+    # TODO: Learn!
+    pass
+
+
+def add_sample():
+    latest_data = requests.get("http://3.138.135.239:3000/data").json()[-1]
+    if isinstance(latest_data["sonar"], list):
+        sonar_data.append(latest_data["sonar"][-1])
+        button_data.append(latest_data["bed_sensor"][-1])
+    elif isinstance(latest_data["sonar"], float):
+        sonar_data.append(latest_data["sonar"])
+        button_data.append(latest_data["bed_sensor"])
+    labels.append(current_label)
+    
+
 @app.route("/calibrate", methods=["POST"])
 def calibrate():
-    sonar = request.form.get("sonar", 0, type=float)
-    bed_sensor = request.form.get("bed_sensor", False, type=bool)
-    sonar_data.append(sonar)
-    sensor_data.append(bed_sensor)
-    print({"sonar": sonar_data, "bed_sensor": sensor_data})
-    return jsonify(sonar_data=str(sonar_data), sensor_data=str(sensor_data))
+    global current_label
+    global samples_per_label
+    global samples_remaining
+    global learning_complete
+
+    if learning_complete:
+        return "{}"
+
+    # First, grab another sample
+    add_sample()
+    samples_remaining -= 1
+
+    # Then, update state
+    if not current_label:
+        if samples_remaining == 0:
+            current_label = True
+            samples_remaining = samples_per_label
+            direction = "Now, get up off of the bed. Continue to press the button. For a robust calibration, press the button from different locations around the room."
+            message = f"Press the button {samples_remaining} more times!"
+        else:
+            message = f"Press the button {samples_remaining} more times!"
+    else:
+        if samples_remaining == 0:
+            learn()
+            learning_complete = True
+            direction = ""
+            message = f"Learning Complete!"
+        else:
+            message = f"Press the button {samples_remaining} more times!"
+    
+    json_kwargs = dict(
+        sonar_data = str(sonar_data),
+        sensor_data = str(button_data),
+        label_data = str(labels),
+        message = message,
+    )
+    if "direction" in locals():
+        json_kwargs["direction"] = direction
+
+    return jsonify(**json_kwargs)
